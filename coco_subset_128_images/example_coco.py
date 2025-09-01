62e574b629bd563b2fcc3fa5a8e89d9779fc60c5
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2025-09-01 09:41:05 +0200

    fix example

--- coco_subset_128_images/example_coco.py
+++ coco_subset_128_images/example_coco.py
@@ -9,7 +9,7 @@ loader = DatasetLoader()
 current_dir = Path(__file__).resolve().parent
 loader.from_coco_instance_segmentations(
     annotations_json_path=str(current_dir / "instances_train2017.json"),
-    input_images_folder=str(current_dir / "images"),
+    img_dir=str(current_dir / "images"),
 )
 
 # We start the UI application on port 8001
